317a16bd12c995a0ce29000f4730b37f41bc8dd1
mathieudutour <mat.dutour@gmail.com>
2019-01-04 11:15:20 +0100

    check if the char pointer conversion worked

--- src/framework/mocha/Utilities/MOFunctionArgument.m
+++ src/framework/mocha/Utilities/MOFunctionArgument.m
@@ -905,9 +905,14 @@ typedef struct { char a; BOOL b; } struct_C_BOOL;
                 return YES;
             }
             
-            // Convert to NSString and then to JavaScript string
-            NSString *name = [NSString stringWithUTF8String:charPtr];
-            JSStringRef    jsName = JSStringCreateWithCFString((__bridge CFStringRef)name);
+            CFStringRef name = CFStringCreateWithCString(NULL, charPtr, kCFStringEncodingUTF8);
+            if (name == NULL) {
+                // for some reason, in some case, the string can be nil. Sight.
+                // https://sketchplugins.com/d/1175-cfarray-to-nsarray/9
+                // Better throw an Error than crash Sketch
+                return NO;
+            }
+            JSStringRef  jsName = JSStringCreateWithCFString(name);
             *value = JSValueMakeString(ctx, jsName);
             JSStringRelease(jsName);
             
